711ebe6c14c537b3917961a59e29dc781ffff15a
steveonodera <estebangrajalescouto@gmail.com>
2026-04-18 12:14:59 -0300

    Create shinobis.js

---
+++ shinobis.js
@@ -0,0 +1,61 @@
+const shinobisDB = {
+    "012607": { 
+        nombre: "Naruto Namikaze", rangoInicial: "Genin", potencial: "S",
+        habilidadesIniciales: ["Uzumaki"], armasIniciales: [], jutsusIniciales: ["Elemento Viento: Gran Ruptura", "Elemento Viento: Palma de Vendaval"], 
+        baseStats: { nin: 2, tai: 1, gen: 0, fue: 1, des: 1, int: 0, vida: 5, energia: 6, chakra: 6, maxJutsus: 4 }, 
+        maxStats:  { nin: 13, tai: 9, gen: 6, fue: 9, des: 10, int: 6, vida: 9, energia: 10, chakra: 7, maxJutsus: 15 } 
+    },
+    "012601": { 
+        nombre: "Sakura Haruno", rangoInicial: "Genin", potencial: "A",
+        habilidadesIniciales: ["Control de Chakra Perfecto", "Inmunidad a Genjutsu Básico"], armasIniciales: [], jutsusIniciales: [], 
+        baseStats: { nin: 0, tai: 0, gen: 1, fue: 0, des: 0, int: 2, vida: 5, energia: 4, chakra: 5, maxJutsus: 3 }, 
+        maxStats:  { nin: 8, tai: 6, gen: 10, fue: 5, des: 7, int: 11, vida: 7, energia: 8, chakra: 7, maxJutsus: 9 } 
+    },
+    "012611": { 
+        nombre: "Shikamaru Nara", rangoInicial: "Genin", potencial: "S",
+        habilidadesIniciales: ["Prodigio", "Fastidio (Misión C/D)", "Estratega Motivado (Misión A/S)"], armasIniciales: [], jutsusIniciales: ["Jutsu de Posesión de Sombra"], 
+        baseStats: { nin: 1, tai: 0, gen: 0, fue: 0, des: 0, int: 3, vida: 4, energia: 4, chakra: 4, maxJutsus: 2 }, 
+        maxStats:  { nin: 9, tai: 6, gen: 7, fue: 6, des: 7, int: 13, vida: 7, energia: 7, chakra: 7, maxJutsus: 10 } 
+    },
+    "012625": { 
+        nombre: "Choji Akimichi", rangoInicial: "Genin", potencial: "A",
+        habilidadesIniciales: ["Cuerpo en Expansión", "Reserva Calórica"], armasIniciales: [], jutsusIniciales: [], 
+        baseStats: { nin: 0, tai: 1, gen: 0, fue: 2, des: 0, int: 0, vida: 5, energia: 4, chakra: 5, maxJutsus: 1 }, 
+        maxStats:  { nin: 7, tai: 10, gen: 3, fue: 11, des: 5, int: 6, vida: 9, energia: 9, chakra: 7, maxJutsus: 5 } 
+    },
+    "012620": { 
+        nombre: "Kiba Inuzuka", rangoInicial: "Genin", potencial: "B",
+        habilidadesIniciales: ["Olfato Salvaje"], armasIniciales: [], jutsusIniciales: [], 
+        baseStats: { nin: 0, tai: 1, gen: 0, fue: 1, des: 1, int: 0, vida: 5, energia: 6, chakra: 4, maxJutsus: 1 }, 
+        maxStats:  { nin: 6, tai: 10, gen: 1, fue: 7, des: 7, int: 3, vida: 7, energia: 9, chakra: 7, maxJutsus: 6 } 
+    },
+    "012618": { 
+        nombre: "Shino Aburame", rangoInicial: "Genin", potencial: "A",
+        habilidadesIniciales: ["Huésped de Insectos", "Analítico"], armasIniciales: [], jutsusIniciales: [], 
+        baseStats: { nin: 1, tai: 0, gen: 0, fue: 0, des: 0, int: 1, vida: 5, energia: 4, chakra: 5, maxJutsus: 2 }, 
+        maxStats:  { nin: 10, tai: 7, gen: 5, fue: 5, des: 5, int: 8, vida: 8, energia: 7, chakra: 8, maxJutsus: 8 } 
+    },
+    "012612": { 
+        nombre: "Hinata Hyuga", rangoInicial: "Genin", potencial: "B",
+        habilidadesIniciales: ["Rama principal Hyuga", "Byakugan Latente", "Timidez"], armasIniciales: [], jutsusIniciales: [], 
+        baseStats: { nin: 1, tai: 2, gen: 0, fue: 0, des: 1, int: 0, vida: 4, energia: 5, chakra: 4, maxJutsus: 2 }, 
+        maxStats:  { nin: 5, tai: 11, gen: 4, fue: 7, des: 10, int: 6, vida: 7, energia: 6, chakra: 8, maxJutsus: 6 } 
+    },
+    "012587": { 
+        nombre: "Neji Hyuga", rangoInicial: "Genin", potencial: "A",
+        habilidadesIniciales: ["Rama secundaria Hyuga", "Prodigio", "Byakugan Latente"], armasIniciales: [], jutsusIniciales: [], 
+        baseStats: { nin: 1, tai: 3, gen: 0, fue: 1, des: 3, int: 1, vida: 4, energia: 5, chakra: 5, maxJutsus: 2 }, 
+        maxStats:  { nin: 7, tai: 14, gen: 10, fue: 8, des: 13, int: 10, vida: 8, energia: 8, chakra: 10, maxJutsus: 8 } 
+    },
+    "012586": { 
+        nombre: "Rock Lee", rangoInicial: "Genin", potencial: "A",
+        habilidadesIniciales: ["Defecto de chakra", "Hachimon: Puertas 1-2", "Tobilleras"], armasIniciales: [], jutsusIniciales: [], 
+        baseStats: { nin: 0, tai: 3, gen: 0, fue: 2, des: 3, int: 0, vida: 5, energia: 7, chakra: 0, maxJutsus: 3 }, 
+        maxStats:  { nin: 0, tai: 15, gen: 0, fue: 13, des: 14, int: 6, vida: 8, energia: 10, chakra: 0, maxJutsus: 6 } 
+    },
+    "012573": { 
+        nombre: "Tenten", rangoInicial: "Genin", potencial: "B",
+        habilidadesIniciales: ["Especialista en Armas", "Precisión Absoluta"], armasIniciales: ["Pergamino de Invocación Ofensivo"], jutsusIniciales: [], 
+        baseStats: { nin: 0, tai: 1, gen: 0, fue: 0, des: 1, int: 1, vida: 4, energia: 4, chakra: 5, maxJutsus: 1 }, 
+        maxStats:  { nin: 6, tai: 8, gen: 5, fue: 6, des: 6, int: 5, vida: 7, energia: 8, chakra: 6, maxJutsus: 6 } 
+    };
